710e6bb09cc67ce5a912f5598dad27a07d8cc57e
FrostedHamster <tanither@hotmail.de>
2016-10-11 15:47:33 +0200

    added user input

--- Calculator.java
+++ Calculator.java
@@ -2,5 +2,9 @@ import java .util.*;
 public class Calculator{
 	public static void man(String[] args){
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Bitte eingeben [+-*/] zahl1 zahl2: ");
+		String operator = scanner.next();
+		int zahl1 = scanner.nextInt();
+		int zahl2 = scanner.nextInt();
 	}
 }
